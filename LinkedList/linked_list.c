d8bcf18d4104ef398cdc5a5f83f3410ddfbdf200
EvoGen3sis <192278290+EvoGen3sis@users.noreply.github.com>
2025-12-29 21:17:02 +0000

    LL

--- LinkedList/linked_list.c
+++ LinkedList/linked_list.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct Node Node;
 
 struct Node {
-    int data;
+    size_t data;
     Node* next;
 };
 typedef struct {
     Node* head;
     Node* tail;
-    size_t capacity;
     size_t size;
 } LList;
 
-LList* create_list(size_t initial_capacity){
+LList* create_list(){
     LList* list = (LList*) malloc(sizeof(LList));
-    if (list == NULL || initial_capacity < 1) {
-        free(list);
-        return NULL;
+    if (list != NULL) {
+        list -> size = 0;
+        list -> head = NULL;
+        list -> tail = NULL;
+        return list;
     }
-    return list;
+    return NULL;
+}
+
+bool push_list(LList* list, size_t data){
+    if (list != NULL){
+
+    }
+    return false;
 }
